f1e20f188c5890ef3ed12c5ecb48bde87d1a25a3
xistianakapsali-cyber <xistianakapsali@gmail.com>
2026-05-31 20:21:39 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+﻿const CACHE_NAME = 'yioio-cache-v1';
+const assets = [
+  './',
+  './index.html',
+  './styles.css',
+  './icon.png',
+  './icon-large.png',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
